5bdaed3da69ab8c3109afb49855ad89b31b281a5
valentinerutto <valentinrutto@gmail.com>
2026-01-30 14:51:57 +0300

    -disable authentication

--- src/main/java/com/comulynx/wallet/rest/api/SecurityConfig.java
+++ src/main/java/com/comulynx/wallet/rest/api/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.comulynx.wallet.rest.api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
@@ -12,4 +15,16 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(12);
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http
+                    .csrf(AbstractHttpConfigurer::disable)
+                    .authorizeHttpRequests(auth -> auth
+                            .anyRequest().permitAll() )
+                    .httpBasic(AbstractHttpConfigurer::disable)
+                    .formLogin(AbstractHttpConfigurer::disable);
+
+            return http.build();
+        }
 }
